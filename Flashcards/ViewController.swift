8dead85ddddda9d15f7e1856c689dd10d6a93884
cdetk <pelt_sign_0m@icloud.com>
2022-10-04 03:17:30 -0400

    lab2

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,5 +21,18 @@ class ViewController: UIViewController {
         frontLabel.isHidden = true
     }
     
+    func updateFlashcard(question: String, answer: String) {
+        frontLabel.text = question
+        backLabel.text = answer
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+    }
 }
 
